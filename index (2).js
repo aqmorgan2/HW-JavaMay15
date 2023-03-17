1949764a76deec5292bde04c0c1eba510405acad
Mr. Arusha Morgan <127910562+aqmorgan2@users.noreply.github.com>
2023-03-17 19:22:13 -0400

    Add files via upload

---
+++ index (2).js
@@ -0,0 +1,40 @@
+const month1 = 23
+//assigned the month to be a constant number between 1 and 12
+
+console.log('month: ', month1)
+let greeting
+
+if (month1 == 1 || month1 == 2 || month1 == 12){
+  //here are the statements on WINTER months
+  console.log('It is cold and the Snowman is happy because it is ')
+    if (month1 == 12)  {console.log('December \n')}
+    else if (month1 == 1) {console.log('January \n')}
+    else if (month1 == 2) {console.log('February \n')}
+  console.log('Its winter time. \n\n')
+} else if (month1 == 3 || month1 == 4 || month1 == 5){
+  console.log('It is SPRING time!  Get your seeds planted, weeds pulled, and a raincoat because it is ')
+  //here are the statements on SPRING months
+  
+    if (month1 == 3)  {console.log('March \n')}
+    else if (month1 == 4) {console.log('April \n')}
+    else if (month1 == 5) {console.log('May \n')}
+
+} else if (month1 == 6 || month1 == 7 || month1 == 8){
+  console.log('It is SUMMER time!  Get your bathing suit, lawnmowers, lemonade, grill, and swim trunks because it is ')
+  //here are the statements on SUMMER months
+  
+    if (month1 == 6 || month1 == 7 || month1 == 8)  {console.log('June \n')}
+    else if (month1 == 7) {console.log('July \n')}
+    else if (month1 == 8) {console.log('August \n')}
+console.log('School is out!!  Vacation Time!!! Beach, sun... here we come. \n\n')
+} else if (month1 == 9 || month1 == 10 || month1 == 11){
+  console.log('It is FALL time!  Get your rake, jackets, wind-breakers because it is ')
+  //here are the statements on FALL months
+  
+    if (month1 == 9) {console.log('September \n')}
+    else if (month1 == 10) {console.log('October \n')}
+    else if (month1 == 11) {console.log('November \n')}
+console.log('School is in its first months/quarter. \n\n')
+} else
+
+  if (month1 > 12 || month1 < 1) {console.log('this is not a valid entry: ', month1)}
